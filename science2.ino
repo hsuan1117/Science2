7a71cc2843846b7ebbaf0175adddf563448658b8
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2021-02-01 14:35:11 +0800

    Update science2.ino

--- science2.ino
+++ science2.ino
@@ -1,14 +1,13 @@
 #include <SoftwareSerial.h>
 #include <Stepper.h>
 /* File: GetMax
- * Copyright (c) 2021 Hsuan,Wei
+   Copyright (c) 2021 Hsuan,Wei
    For more details, see https://www.sivir.pw , https://hsuan.app
    Released under the MIT license.
  * */
 
 /**
    旋轉延時 <br>
-
    值越小轉越快，太小會轉不動
  * */
 const int MOTOR_STEP = 2048;
@@ -16,14 +15,11 @@ const int MOTOR_STEP = 2048;
 
 /**
    亮度感測器 <br>
-
    <li><b>8</b> CT ==> uart_tx(mode 1)</li>
    <li><b>9</b> DR ==> uart_rx(mode 1)</li>
    接口和arduino的 rx tx 要相反
-
    @param rx : int 接收資料端口
    @param tx : int 重送資料端口
-
    @see <a href="https://www.arduino.cc/en/Reference/SoftwareSerialConstructor">說明文件</a>
  **/
 SoftwareSerial light(10, 9);
@@ -40,65 +36,63 @@ byte data[32];
  * */
 class GY39 {
 
-private:
+  private:
     static bool verify_data(int length, int start) {
-        byte btmp = 0;
-        for (int i = 0; i < length; ++i) {
-            btmp += data[start + i];
-        }
-        if (btmp == data[start + length - 1])
-            return true;
-        return false;
+      byte btmp = 0;
+      for (int i = 0; i < length; ++i) {
+        btmp += data[start + i];
+      }
+      if (btmp == data[start + length - 1])
+        return true;
+      return false;
     }
 
-public:
+  public:
     static const byte FRAME_FLAG = 0x5A;
     static const byte FRAME_DATATYPE_LIGHT = 0x15;
     static const byte FRAME_DATATYPE_WET = 0x45;
     static const byte FRAME_DATATYPE_IIC = 0x55;
 
     /***
-
-
        @see
         <a href="https://www.taiwaniot.com.tw/wp-content/uploads/woocommerce_uploads/2016/10/GY39_manual.pdf" >GY39 說明文件</a>
        @return
     */
     static double calculate() {
-        int start;
-        //delay(10);
+      int start;
+      //delay(10);
 
-        //尋找資料起始點
-        for (int i = 0; i < 26; ++i) {
-            if (data[i] == FRAME_FLAG && data[i + 1] == FRAME_FLAG) {
-                Serial.println("[Info] Got sensor data.");
-                start = i;
-                break;
-            }
+      //尋找資料起始點
+      for (int i = 0; i < 26; ++i) {
+        if (data[i] == FRAME_FLAG && data[i + 1] == FRAME_FLAG) {
+          Serial.println("[Info] Got sensor data.");
+          start = i;
+          break;
         }
+      }
 
-        delay(100);
-        if (data[start + 2] == FRAME_DATATYPE_LIGHT) {
-            Serial.println("[Info] DataType: Light");
-            verify_data(8, start);
+      delay(100);
+      if (data[start + 2] == FRAME_DATATYPE_LIGHT) {
+        Serial.println("[Info] DataType: Light");
+        verify_data(8, start);
 
-            Serial.print("[Info] Light = ");
-            double lux = (data[start + 4] << 24) + (data[start + 5] << 16) + (data[start + 6] << 8) + (data[start + 7]);
-            Serial.println(abs(lux / 100.0));
-            return abs(lux / 100.0);
+        Serial.print("[Info] Light = ");
+        double lux = (data[start + 4] << 24) + (data[start + 5] << 16) + (data[start + 6] << 8) + (data[start + 7]);
+        Serial.println(abs(lux / 100.0));
+        return abs(lux / 100.0);
 
-        } else {
-            Serial.println("[Error] Method not allowed.");
-        }
+      } else {
+        Serial.println("[Error] Method not allowed.");
+      }
     }
 
     void read_data() {
-        for (int i = 0; i < 25; ++i) {
-            data[i] = light.read();
-            Serial.print(data[i]);
-            Serial.print(" ");
-        }
-        Serial.print('\n');
+      for (int i = 0; i < 25; ++i) {
+        data[i] = light.read();
+        Serial.print(data[i]);
+        Serial.print(" ");
+      }
+      Serial.print('\n');
     }
 
 
@@ -111,82 +105,86 @@ GY39 Device;
    初始化Arduino板設定
  * */
 void setup() {
-    pinMode(A0, OUTPUT);
-    pinMode(A1, OUTPUT);
-    pinMode(A2, OUTPUT);
-    pinMode(A3, OUTPUT);
-    pinMode(8, OUTPUT);
-    pinMode(11, OUTPUT);
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(11, OUTPUT);
 
-    /// 初始化Arduino 序列阜
-    Serial.begin(115200);
-    light.begin(9600);
+  /// 初始化Arduino 序列阜
+  Serial.begin(115200);
+  light.begin(9600);
 
-    motor.setSpeed(5);
-    digitalWrite(11, HIGH);
-    digitalWrite(8, LOW);
+  motor.setSpeed(5);
+  digitalWrite(11, HIGH);
+  digitalWrite(8, LOW);
 
-    Serial.print("Start");
-    // 亮度感測器模式
-    byte mode[3] = {0xA5, 0x81, 0x26};
-    for (int i = 0; i < 3; ++i) {
-        light.print(mode[i]);
-    }
+  Serial.print("Start");
+  // 亮度感測器模式
+  byte mode[3] = {0xA5, 0x81, 0x26};
+  for (int i = 0; i < 3; ++i) {
+    light.print(mode[i]);
+  }
 }
 String payload;
 long long start;
 bool started = false;
 
 void loop() {
-    delay(500);
-    if(Serial.find('!')){
-        payload = Serial.readStringUntil('!');
-        Serial.print("[Debug] ");
-        Serial.println(payload);
+  //delay(100);
+  if (Serial.find('@')) {
+    payload = Serial.readStringUntil('!');
+    Serial.print("[Debug] ");
+    Serial.println(payload);
 
+    /* @0:1! */
+    int method = getValue(payload, ':', 0).toInt();
+    int step   = getValue(payload, ':', 1).toInt();
+    Serial.println(method);
+    Serial.println(step);
+    //while (Serial.available()) {
+    switch (method) {
+      case 0:
+        //開始
+        start = millis();
+        started = true;
+        break;
+      case 1:
+        started = false;
+        break;
 
-        int method = getValue(payload, ':', 0).toInt();
-        int step   = getValue(payload, ':', 1).toInt();
-        //while (Serial.available()) {
-        switch (method) {
-            case 0:
-                //開始
-                start = millis();
-                started = true;
-                break;
-            case 1:
-                started = false;
-                break;
-
-            case 2:
-                for (int i = 1; i <= step; i++) {
-                    motor.step(1);
-                }
-                break;
+      case 2:
+        for (int i = 1; i <= step; i++) {
+          motor.step(1);
         }
-
-        if (started) {
-            Device.read_data();
-            Serial.print("Time: ");
-            Serial.println(millis() - start);
-
-            Serial.print("Lux : ");
-            Serial.println(Serial.print(Device.calculate()));
-        }
+        break;
     }
+
+  }
+
+  if (started) {
+    Device.read_data();
+    Serial.print("Time: ");
+    int t = millis() - start;
+    Serial.println(t);
+
+    Serial.print("Lux : ");
+    Serial.println(Serial.print(Device.calculate()));
+  }
 }
 
 String getValue(String data, char separator, int index) {
-    int found = 0;
-    int strIndex[] = {0, -1};
-    int maxIndex = data.length() - 1;
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length() - 1;
 
-    for (int i = 0; i <= maxIndex && found <= index; i++) {
-        if (data.charAt(i) == separator || i == maxIndex) {
-            found++;
-            strIndex[0] = strIndex[1] + 1;
-            strIndex[1] = (i == maxIndex) ? i + 1 : i;
-        }
+  for (int i = 0; i <= maxIndex && found <= index; i++) {
+    if (data.charAt(i) == separator || i == maxIndex) {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
     }
-    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
